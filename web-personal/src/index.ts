ead43a597f0baf3736dfaf3d69ed83061794e8dc
Francisco Figari <fran@urbanly.org>
2025-10-10 17:15:55 -0300

    add web personal content

--- web-personal/src/index.ts
+++ web-personal/src/index.ts
@@ -1,9 +1,31 @@
+import path from "path";
+import express, { Express } from "express";
+
 import { createHTTPServer } from "@pranidhana/lib";
 
 const name = "web-personal"
 
+export const createWebPersonalServer = (): Express => {
+	const app = createHTTPServer()
+
+	const pagesDir = path.resolve(__dirname, "../pages");
+	app.use(express.static(pagesDir));
+
+	app.get("/", (_, res) => res.redirect("/i"));
+
+	app.get("/i", (_, res) => {
+		res.sendFile(path.resolve("./pages/i.html"))
+	})
+
+	app.get("/favicon.ico", (_, res) => {
+		res.sendStatus(204)
+	})
+
+	return app
+}
+
 const port = 3000
 
-createHTTPServer().listen(port, () => {
+createWebPersonalServer().listen(port, () => {
 	console.log(`starting "${name}" at port ${port}`)
 })
